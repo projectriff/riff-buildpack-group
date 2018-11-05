07f717b2ae76d8950043641a9ba6e0e1ccb018bb
Eric Bottard <ebottard@pivotal.io>
2018-11-05 14:10:48 +0100

    Don't push as 'latest' for now

--- .travis/push-to-dockerhub.sh
+++ .travis/push-to-dockerhub.sh
@@ -9,4 +9,5 @@ docker tag projectriff/buildpack:latest projectriff/buildpack:${version}
 docker tag projectriff/buildpack:latest projectriff/buildpack:${version}-ci-${TRAVIS_COMMIT}
 
 docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
-docker push projectriff/buildpack
+docker push projectriff/buildpack:${version}
+docker push projectriff/buildpack:${version}-ci-${TRAVIS_COMMIT}
