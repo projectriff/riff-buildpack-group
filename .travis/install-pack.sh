5787d2ba2f28629fe03d6b08d520fd4a7370616f
Scott Andrews <sandrews@pivotal.io>
2019-04-02 22:38:56 -0400

    pack@0.1.0

--- .travis/install-pack.sh
+++ .travis/install-pack.sh
@@ -4,9 +4,5 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# TODO use a released version of pack once there is a release to consume
-# wget -qO- https://github.com/buildpack/pack/releases/download/v0.1.0/pack-0.1.0-linux.tar.gz | tar xvz -C $HOME/bin
-# export PATH="$HOME/bin:$PATH"
-
-# master as of 2019-04-02
-GO111MODULE=on go get github.com/buildpack/pack/cmd/pack@d8ba851f1a0d9181f4265239bead7ab1fa6882c7
+wget -qO- https://github.com/buildpack/pack/releases/download/v0.1.0/pack-v0.1.0-linux.tgz | tar xvz -C $HOME/bin
+export PATH="$HOME/bin:$PATH"
