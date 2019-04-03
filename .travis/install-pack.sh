37569db267312b4d751fe124cf547726cb73210b
Scott Andrews <sandrews@pivotal.io>
2019-04-02 21:49:05 -0400

    Prep upgrade to pack 0.1.0
    - consume 0.1.0 pack lifecycle images
    - consume the latest pack master commit (not yet 0.1.0)
    
    Refs projectriff/riff#1139

--- .travis/install-pack.sh
+++ .travis/install-pack.sh
@@ -5,8 +5,8 @@ set -o nounset
 set -o pipefail
 
 # TODO use a released version of pack once there is a release to consume
-# wget -qO- https://github.com/buildpack/pack/releases/download/v0.0.8/pack-0.0.8-linux.tar.gz | tar xvz -C $HOME/bin
+# wget -qO- https://github.com/buildpack/pack/releases/download/v0.1.0/pack-0.1.0-linux.tar.gz | tar xvz -C $HOME/bin
 # export PATH="$HOME/bin:$PATH"
 
-# master as of 2019-03-21
-GO111MODULE=on go get github.com/buildpack/pack/cmd/pack@ebc7c99fc236c4757c6138dfe9234e575f56fb18
+# master as of 2019-04-02
+GO111MODULE=on go get github.com/buildpack/pack/cmd/pack@d8ba851f1a0d9181f4265239bead7ab1fa6882c7
