8b09bbf875394ee3e1aae433755a8af6c1d1c804
Scott Andrews <sandrews@pivotal.io>
2019-07-22 16:05:41 -0400

    Add a warning to not edit the output file

--- apply-template.sh
+++ apply-template.sh
@@ -9,6 +9,7 @@ output=${2}
 
 if [ -f $template ] ; then
   rm  $output
+  echo "# DO NOT EDIT - this file is the output of the '$template' template " >> $output
   while IFS= read -r line
   do
     eval "echo \"$(echo "$line" | sed -e 's|"|\\"|g')\" >> $output"
